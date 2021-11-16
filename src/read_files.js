05aa8d7823e77727f031d794dbec453eafc8bd82
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2021-11-16 14:48:24 -0500

    update spectral action

--- src/read_files.js
+++ src/read_files.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const fg = require('fast-glob');
 
 const readFilesToAnalyze = async (githubWorkspace, fileGlob) => {
+  for (let i = 0, len = fileGlob.length; i < len; i++) {
+    console.log(`File ${i}:  ${fileGlob[i]}`)
+  } 
+
+
   console.dir("FileGlob: " + fileGlob);
   const entries = await fg(fileGlob);
 
