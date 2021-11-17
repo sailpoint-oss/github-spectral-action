e0708904381af81426679d44cd1fad97bcea3d49
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2021-11-17 14:57:41 -0500

    fix iterator

--- src/read_files.js
+++ src/read_files.js
@@ -4,7 +4,7 @@ const readFilesToAnalyze = async (githubWorkspace, fileGlob) => {
   let files = fileGlob.split(",");
   let fileContents = [];
   for (let i = 0, len = files.length; i < len; i++) {
-    console.log(`FILE ${j} ${files[j]}`);
+    console.log(`FILE ${i} ${files[i]}`);
     fileContents.push({
       file: files[i],
       content: fs.readFileSync(files[i], 'utf-8')
